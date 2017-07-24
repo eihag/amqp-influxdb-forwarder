caf5fcb554fc85c1e1f586e14417200fad46fc82
Eivind Hagen <eh@hagen.info>
2017-07-24 22:46:42 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # amqp-influxdb-forwarder
-Read messages from Azure IoThub and forward to InfluxDB
+Small utility to read messages from Azure IoThub and forward to InfluxDB.
+
+Flow:
+1) Receive events from Azure IotHub using AMQP
+2) Convert from IotHub format to InfluxDB JSON format and ensure (custom) properties are in payload
+3) Write new message to influxdb
+
+
+
+In case of (network) errors: 
+1. sleep for 30 seconds
+2. retry
+3. still an error? goto 1
+
+
+If restarted it will read all messages from the beginning of the queue. InfluxDB will ignore duplicate entries.
+
+
+Packaged with Docker. Image available here: https://hub.docker.com/r/eihag/amqp-influxdb-forwarder/
+
+
+### Build
+<pre>
+docker build -t amqp-influxdb-forwarder:latest . 
+</pre>
+
+### Test run
+<pre>
+docker run -d --name influxdb -p 8083:8083 -p 8086:8086 influxdb
+docker run --net container:influxdb  -v $(pwd)/config.properties:/config.properties amqp-influxdb-forwarder 
+</pre>
+
+
+
+### Local OSX test AMQP client
+<pre>
+brew install openssl
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+
+pip3 install python-qpid-proton
+</pre>
+
+### Azure and AMQP
+Info about amqp settings: https://github.com/Azure/azure-event-hubs-python.git
+
+
+### Docker housekeeping
+
+Remove dangling images
+<pre>
+docker rmi $(docker images -f "dangling=true" -q)
+</pre>
+
+Attach to running container
+<pre>
+docker exec -i -t influxdb /bin/bash 
+</pre>
---
+++ readIotHubAmqpClient.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from proton.reactor import Container, Selector
+from proton.handlers import MessagingHandler
+from influxdb import InfluxDBClient
+
+import configparser
+import json
+import logging
+import time
+
+# Config file must be bound to docker image at runtime
+CONFIG_FILE_PATH = 'config.properties'
+
+
+FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+logger = logging.getLogger(__name__)
+
+configp = configparser.ConfigParser()
+configp.read(CONFIG_FILE_PATH)
+
+
+influx_config = configp['influxdb']
+
+DB_NAME = influx_config['DATABASE']
+logging.info("Using database: '{0}'".format(DB_NAME))
+
+influxdb_client = InfluxDBClient(influx_config['HOSTNAME'],
+                                 influx_config['PORT'],
+                                 influx_config['USER'],
+                                 influx_config['PASSWORD'],
+                                 DB_NAME)
+
+while True:
+    try:
+        dbs = influxdb_client.get_list_database()
+        if DB_NAME not in dbs:
+            influxdb_client.create_database(DB_NAME)
+    except Exception as e:
+        logger.exception("Error connecting to InfluxDB. Retrying in 30sec")
+        time.sleep(30)
+        continue
+    else:
+        logging.info("connected to influxdb")
+        break
+
+
+class Receiver(MessagingHandler):
+    def __init__(self):
+        super(Receiver, self).__init__()
+
+    def on_start(self, event):
+        # -1 = beginning
+        #  @latest = only new messages
+        offset = "-1"
+        selector = Selector(u"amqp.annotation.x-opt-offset > '" + offset + "'")
+
+        azure_config = configp['azure']
+        amqp_url = azure_config['IOTHUB_AMQP_URL']
+        partition_name = azure_config['IOTHUB_PARTITION_NAME']
+
+        while True:
+            try:
+                conn = event.container.connect(amqp_url, allowed_mechs="PLAIN")
+                event.container.create_receiver(conn, partition_name + "/ConsumerGroups/$default/Partitions/0",
+                                                options=selector)
+                event.container.create_receiver(conn, partition_name + "/ConsumerGroups/$default/Partitions/1",
+                                                options=selector)
+            except Exception as e:
+                logger.exception("Error connecting to IotHub. Retrying in 30sec")
+                time.sleep(30)
+                continue
+            else:
+                break
+        logging.info("Setup complete")
+
+    def on_message(self, event_received):
+        logging.info("Event received: '{0}'".format(event_received.message))
+
+        payload = convert_to_influx_format(event_received.message)
+
+        if payload is not None:
+            logging.info("Write points: {0}".format(payload))
+            while True:
+                try:
+                    influxdb_client.write_points(payload)
+                except Exception as e:
+                    logger.exception("Error writing to InfluxDB. Retrying in 30sec")
+                    time.sleep(30)
+                    continue
+                else:
+                    break
+
+
+def convert_to_influx_format(message):
+    name = message.annotations["iothub-connection-device-id"]
+    try:
+        json_input = json.loads(message.body)
+    except json.decoder.JSONDecodeError:
+        return
+
+    if 'temperature_C' not in json_input or 'humidity' not in json_input or 'battery' not in json_input:
+        logging.info('Ignoring event in unknown format')
+        return
+
+    time = json_input["time"]
+    temperature = json_input["temperature_C"]
+    humidity = json_input["humidity"]
+    battery_status = json_input["battery"]
+
+    json_body = [
+        {'measurement': name, 'time': time, 'fields': {
+            "temperature": temperature,
+            "humidity": humidity,
+            "battery": battery_status
+        }}
+    ]
+
+    return json_body
+
+
+def main():
+    try:
+        Container(Receiver()).run()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
